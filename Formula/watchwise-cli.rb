2a118251d0e1377fdc73913d68412a33e24eec93
Radu Ursache <3800336+rursache@users.noreply.github.com>
2026-03-21 20:42:32 +0200

    Update watchwise-cli to v1.0.1

--- Formula/watchwise-cli.rb
+++ Formula/watchwise-cli.rb
@@ -1,8 +1,8 @@
 class WatchwiseCli < Formula
   desc "Smart movie & TV show recommender powered by your IMDB ratings"
   homepage "https://github.com/rursache/watchwise-cli"
-  url "https://github.com/rursache/watchwise-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "5428d50eead7a88259a6b36a57ea693d1de82d358ee582b583f2e6fcf690dc6c"
+  url "https://github.com/rursache/watchwise-cli/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "a5dfa87bfa3e6e7386d9df954f2f51f838544411eab18e619912a09805dfe8a1"
   license "MIT"
 
   head "https://github.com/rursache/watchwise-cli.git", branch: "master"
