7acac02435fa24b95522803635347688a9b77055
Radu Ursache <3800336+rursache@users.noreply.github.com>
2026-03-21 20:45:01 +0200

    Update watchwise-cli to v1.0.2, fix binary name to watchwise

--- Formula/watchwise-cli.rb
+++ Formula/watchwise-cli.rb
@@ -1,8 +1,8 @@
 class WatchwiseCli < Formula
   desc "Smart movie & TV show recommender powered by your IMDB ratings"
   homepage "https://github.com/rursache/watchwise-cli"
-  url "https://github.com/rursache/watchwise-cli/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "a5dfa87bfa3e6e7386d9df954f2f51f838544411eab18e619912a09805dfe8a1"
+  url "https://github.com/rursache/watchwise-cli/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "6fad2ccff1d408529d1265f37c26cda2f26d6c74514c6b39c6ef2401e8dd5c0d"
   license "MIT"
 
   head "https://github.com/rursache/watchwise-cli.git", branch: "master"
@@ -10,10 +10,10 @@ class WatchwiseCli < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "."
+    system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}", output: bin/"watchwise"), "."
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/watchwise-cli -v")
+    assert_match version.to_s, shell_output("#{bin}/watchwise -v")
   end
 end
